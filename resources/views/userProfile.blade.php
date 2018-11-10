bbf1c8058792384130ae9823a05a1a474e1f1abf
zareen27 <zareenfariha027@gmail.com>
2018-11-10 11:07:59 +0600

    new change

--- resources/views/userProfile.blade.php
+++ resources/views/userProfile.blade.php
@@ -3,6 +3,36 @@
 @section('content')
 
 <h1 class='font-italic text-success'> Welcome User!</h1>
+<div class="jumbotron bg-light">
+  <!--<h1 class="display-4 text-success">WELCOME!</h1>-->
+   <hr>
+  
+    <br>
+    <p class="text-info">With a view to reducing troubles of travelling we provide an online solution to ease the hassles of shopping.</p>
+
+
+<hr>
+
+<legend class="text-success">Our facilities</legend>
+
+<li class="text-info">Pet Delivery</li>
+<li class="text-info">Secured Payments.
+</li>
+<li class="text-info">Assurance.</li>
+<li class="text-info">Discounts.</li>
+<li class="text-info">Product Quality Guaranteed.</li>
+<li class="text-info">We also offer additional requirements for the product you buy**. </li>
+
+</p>
+  <hr class="my-4">
+ 
+  <p class="lead">
+    <a class="btn btn-info btn-lg" href="#" role="button">Learn more</a>
+  </p>
+</div>
+
+
+
 
 
 @endsection
